cffa6e0ffbd1c686806da8121b26d60dde5bfcdd
LevyvoNet <levyvonet@Elad-PC.localdomain>
2020-02-08 16:43:45 +0200

    added merging of loans to a single dataframe

--- mortgage.py
+++ mortgage.py
@@ -1,9 +1,12 @@
 import pandas as pd
+import numpy as np
+from collections import namedtuple
+
+Loan = namedtuple('Loan', ['fund', 'year_interest', 'month_count'])
 
 
-def calc_paybacks(fund, year_interest, period_years, calc_interest_and_fund_func):
+def calc_paybacks(fund, year_interest, months_count, calc_interest_and_fund_func):
     month_interest = year_interest / 12.0
-    months_count = period_years * 12
     curr_balance = fund
     paybacks_df = pd.DataFrame(columns=['fund', 'interest', 'total', 'balance'])
     while (curr_balance > 0):
@@ -28,8 +31,8 @@ def const_fund_calc_interest_and_fund(fund, month_interest, months_count, curr_b
     return curr_fund, curr_interest
 
 
-def const_fund(fund, year_interest, period_years):
-    return calc_paybacks(fund, year_interest, period_years, const_fund_calc_interest_and_fund)
+def const_fund(fund, year_interest, months_count):
+    return calc_paybacks(fund, year_interest, months_count, const_fund_calc_interest_and_fund)
 
 
 def shpitzer_calc_interest_and_fund(fund, month_interest, months_count, curr_balance):
@@ -40,5 +43,16 @@ def shpitzer_calc_interest_and_fund(fund, month_interest, months_count, curr_bal
     return curr_fund, curr_interest
 
 
-def shpitzer(fund, year_interest, period_years):
-    return calc_paybacks(fund, year_interest, period_years, shpitzer_calc_interest_and_fund)
+def shpitzer(fund, year_interest, month_count):
+    return calc_paybacks(fund, year_interest, month_count, shpitzer_calc_interest_and_fund)
+
+
+def shptizer_multiple(loans):
+    dfs = [shpitzer(loan.fund, loan.year_interest, loan.month_count) for loan in loans]
+    max_period = max([loan.month_count for loan in loans])
+    dfs_extended = [pd.concat([df, pd.DataFrame(np.zeros((max_period - len(df), df.shape[1])),
+                                                columns=dfs[0].columns,
+                                                index=range(len(df), max_period))])
+                    for df in dfs]
+
+    return sum(dfs_extended)
